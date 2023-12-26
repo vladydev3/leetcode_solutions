daa267d2d752b78eb57236917908daf7981cf9f8
Vladimir Pinera <vladypv3@gmail.com>
2023-12-25 22:54:36 -0500

    recover-binary-search-tree

---
+++ c#/trees/recover-binary-search-tree/Program.cs
@@ -0,0 +1,74 @@
+﻿// You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void RecoverTree(TreeNode root)
+    {
+        var nodes = InOrderTraversal(root).ToList();
+        TreeNode first = null;
+        TreeNode second = null;
+        int idx = 0;
+
+        for (var i = 0; i < nodes.Count - 1; i++)
+        {
+            if (nodes[i].val > nodes[i + 1].val)
+            {
+                if (first == null)
+                {
+                    first = nodes[i];
+                    idx = i + 1;
+                }
+                second = nodes[i + 1];
+            }
+        }
+
+        if (first != null && second != null)
+        {
+            Swap(first, second);
+        }
+        if (first != null && second == null)
+        {
+            Swap(first, nodes[idx]);
+        }
+    }
+
+    private IEnumerable<TreeNode> InOrderTraversal(TreeNode root)
+    {
+        if (root == null)
+        {
+            yield break;
+        }
+
+        foreach (var node in InOrderTraversal(root.left))
+        {
+            yield return node;
+        }
+
+        yield return root;
+
+        foreach (var node in InOrderTraversal(root.right))
+        {
+            yield return node;
+        }
+    }
+
+    private void Swap(TreeNode first, TreeNode second)
+    {
+        var temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+}
---
+++ c#/trees/unique-binary-search-trees/Program.cs
@@ -0,0 +1,29 @@
+﻿// Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+// Example 1:
+// Input: n = 3
+// Output: 5
+
+// Example 2:
+// Input: n = 1
+// Output: 1
+
+public class Solution
+{
+    public int NumTrees(int n)
+    {
+        int[] catalan = new int[n + 1];
+        catalan[0] = catalan[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++)
+            {
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+            }
+        }
+
+        return catalan[n];
+    }
+}
