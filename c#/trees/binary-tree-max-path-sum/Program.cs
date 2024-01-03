d0a9c3afc7a0fe3960cf5a88c2c5b1443cd5b77a
Vladimir Pinera <vladypv3@gmail.com>
2024-01-03 10:42:36 -0500

    binary-tree-max-path-sum

---
+++ c#/trees/binary-tree-max-path-sum/Program.cs
@@ -0,0 +1,26 @@
+﻿class Solution
+{
+    private int maxSum = int.MinValue;
+
+    public int MaxPathSum(TreeNode root)
+    {
+        MaxGain(root);
+        return maxSum;
+    }
+
+    private int MaxGain(TreeNode node)
+    {
+        if (node == null) return 0;
+
+        // Calculate the maximum sum on the left and right subtrees
+        int leftGain = Math.Max(MaxGain(node.left), 0);
+        int rightGain = Math.Max(MaxGain(node.right), 0);
+
+        // Update maxSum if it's smaller than the new path sum
+        int newPathSum = node.val + leftGain + rightGain;
+        maxSum = Math.Max(maxSum, newPathSum);
+
+        // For the recursion return only the max gain the node and one of its subtrees could add
+        return node.val + Math.Max(leftGain, rightGain);
+    }
+}
