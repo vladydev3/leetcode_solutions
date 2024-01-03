6a17254eff4c6afbbe565539f123d7ebf96ae329
Vladimir Pinera <vladypv3@gmail.com>
2024-01-02 19:09:44 -0500

    .

---
+++ c#/trees/lowest-common-ancestor-of-a-binary-tree/Program.cs
@@ -0,0 +1,43 @@
+﻿//   Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        var q = root.right.left;
+        var p = root.right.right;
+
+        var result = LowestCommonAncestor(root, p, q);
+        Console.WriteLine(result.val);
+    }
+    public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null) return null;
+
+        if (root == p || root == q) return root;
+
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) return root;
+
+        return left != null ? left : right;
+    }
+}
---
+++ c#/trees/remove-invalid-parenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Solution {
+    public IList<string> RemoveInvalidParentheses(string s) {
+        List<string> res = new List<string>();
+        if (s == null) return res;
+
+        HashSet<string> visited = new HashSet<string>();
+        Queue<string> queue = new Queue<string>();
+
+        queue.Enqueue(s);
+        visited.Add(s);
+
+        bool found = false;
+
+        while(queue.Count != 0) {
+            s = queue.Dequeue();
+
+            if (IsValid(s)) {
+                res.Add(s);
+                found = true;
+            }
+
+            if (found) continue;
+
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] != '(' && s[i] != ')') continue;
+
+                string t = s.Substring(0, i) + s.Substring(i + 1);
+
+                if (!visited.Contains(t)) {
+                    queue.Enqueue(t);
+                    visited.Add(t);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public bool IsValid(string s) {
+        int count = 0;
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+            if (c == '(') count++;
+            else if (c == ')' && count-- == 0) return false;
+        }
+        return count == 0;
+    }
+}
