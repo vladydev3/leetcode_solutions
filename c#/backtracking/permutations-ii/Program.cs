41972eac41aad90af2e0ba1508bae949931f61ca
Vladimir Pinera <vladypv3@gmail.com>
2024-01-04 14:46:53 -0500

    .

---
+++ c#/backtracking/permutations-ii/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        List<IList<int>> results = [];
+        List<int> perm = [];
+        Dictionary<int, int> count = [];
+
+        foreach (var num in nums)
+        {
+            if (count.ContainsKey(num))
+            {
+                count[num] += 1;
+            }
+            else
+            {
+                count[num] = 1;
+            }
+        }
+
+        void DFS()
+        {
+            if (perm.Count == nums.Length)
+            {
+                List<int> result = [];
+                foreach (var item in perm)
+                {
+                    result.Add(item);
+                }
+                results.Add(result);
+            }
+            foreach (int key in count.Keys)
+            {
+                if (count[key] > 0)
+                {
+                    perm.Add(key);
+                    count[key] -= 1;
+                    DFS();
+                    count[key] += 1;
+                    perm.RemoveAt(perm.Count - 1);
+                }
+            }
+        }
+
+        DFS();
+        return results;
+    }
+}
